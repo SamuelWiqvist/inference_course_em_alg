cec5ece819483e1e8b4d71d5e7fb8299288380b0
Samuel <swiqvist@gmail.com>
2019-11-19 15:01:57 +0100

    em run

--- em_algorithm.jl
+++ em_algorithm.jl
@@ -48,15 +48,30 @@ function em_alorithm(y::Vector, θ_0::Vector, nbr_iter::Int)
 end
 
 
-y = [40; 27; 24; 9]
-θ_0 = [1/3; 1/3; 1/3]
-N = 10
+# run em algorithm
+y = [40; 27; 24; 9] # data
+θ_0 = [1/3; 1/3; 1/3] # start value for parameters
+N = 10 # number iterations of the EM algorithm
 
 θ_hat, θ_matrix = em_alorithm(y, θ_0, N)
 
 
+round.(θ_hat, digits= 2)
+
+# plot em trace
+
+θ_1 = [1/3; 1/3; 1/3]
+θ_2 = [0.2; 0.2; 0.6]
+θ_3 = [0.8; 0.1; 0.1]
+
+θ_hat_1, θ_matrix_1 = em_alorithm(y, θ_1, N)
+θ_hat_2, θ_matrix_2 = em_alorithm(y, θ_2, N)
+θ_hat_3, θ_matrix_3 = em_alorithm(y, θ_3, N)
+
 PyPlot.figure()
-PyPlot.scatter3D(θ_matrix[1,:], θ_matrix[2,:],θ_matrix[3,:], "*")
+PyPlot.scatter3D(θ_matrix_1[1,:], θ_matrix_1[2,:],θ_matrix_1[3,:], color="blue", "*")
+PyPlot.scatter3D(θ_matrix_2[1,:], θ_matrix_2[2,:],θ_matrix_2[3,:], color="red", "*")
+PyPlot.scatter3D(θ_matrix_3[1,:], θ_matrix_3[2,:],θ_matrix_3[3,:], color="green", "*")
 PyPlot.xlabel("p", fontsize=12)
 PyPlot.ylabel("q", fontsize=12)
 PyPlot.zlabel("r", fontsize=12)
